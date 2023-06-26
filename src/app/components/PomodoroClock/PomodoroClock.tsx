d190fff75b935097edc9978c5af076c29a31f9c1
viniciosragazzidev <devviniciosragazzi@gmail.com>
2023-06-26 16:48:00 -0300

    Feat: Adicionado a função de iniciar e Resetar o pomodoro

--- src/app/components/PomodoroClock/PomodoroClock.tsx
+++ src/app/components/PomodoroClock/PomodoroClock.tsx
@@ -1,55 +1,122 @@
-import { useEffect, useState } from "react";
+import {
+  ClockClockwise,
+  Pause,
+  Play,
+  RewindCircle,
+} from "@phosphor-icons/react";
 
-type Props = {
-  totalMinutos: number;
-};
+import { useState } from "react";
 
 const PomodoroClock = () => {
-  const [totalMinutos, setTotalMinutos] = useState<number>(0.3);
+  const [totalMinutos, setTotalMinutos] = useState<number>(1);
   const [degAtual, setDegAtual] = useState<number>(0);
-  const [tempoAtual, setTempoAtual] = useState<string>("");
-  const [segundosRestantes, setSegundosRestantes] = useState<number>(0);
-  const [iniciado, setIniciado] = useState<boolean>(false);
-  const cronometro = (minutos: number) => {
-    const segundos = minutos * 60;
-    setSegundosRestantes(segundos);
-    let dgA = 0;
-    let segRes = segundos;
 
-    if (!iniciado) {
-        setIniciado(true);
-      const timer = setInterval(() => {
-        dgA += 360 / segundos;
-        segRes -= 1;
+  const [segundosRestantes, setSegundosRestantes] = useState<number>(
+    totalMinutos * 60
+  );
 
-        if (dgA <= 360) {
-          setDegAtual(dgA);
-          setSegundosRestantes(segRes);
-        } else {
-          clearInterval(timer);
-          setIniciado(false);
-        }
-      }, 1000);
+  const [paused, setPaused] = useState<boolean>(false);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null); // Variável para armazenar o intervalo
+
+  const reset = () => {
+    setDegAtual(0);
+
+    setSegundosRestantes(totalMinutos * 60);
+
+    setPaused(false);
+
+    if (timer) {
+      clearInterval(timer);
+      setTimer(null); // Limpa o intervalo se existir
+    }
+  };
+  const start = () => {
+    setPaused(true);
+    if (!timer) {
+      // Inicia um novo intervalo somente se não existir
+      let dgA = 0;
+      let segRes = totalMinutos * 60;
+      cronometro(dgA, segRes);
+    } else {
+      cronometro(degAtual, segundosRestantes);
+    }
+  };
+  const pause = () => {
+    setPaused(false);
+    if (timer) {
+      clearInterval(timer);
     }
   };
 
+  const cronometro = (dgA: number, segRes: number) => {
+    const segundos = totalMinutos * 60;
 
+    const timer = setInterval(() => {
+      dgA += 360 / segundos;
+
+      if (dgA <= 360) {
+        segRes -= 1;
+
+        setDegAtual(dgA);
+        setSegundosRestantes(segRes);
+      } else {
+        reset();
+      }
+    }, 1000);
+    setTimer(timer);
+  };
+  const getTempoAtual = () => {
+    const minutos = Math.floor(segundosRestantes / 60);
+    const segundos = segundosRestantes % 60;
+    const segFormated = ` ${segundos} seg`;
+    const minFormated = minutos === 0 ? '' : ` ${minutos} min`;
+    return `${minFormated}  ${segFormated}`;
+  };
   return (
     <div className="pomodoro-clock w-full h-full flex flex-col gap-4 justify-center items-center max-md:py-10">
+      <div className="flex"></div>
       <div
         className="circular-progress w-64 h-64 relative rounded-full bg-[#060314] flex items-center justify-center before:w-56 before:h-56 before:absolute before:bg-black before:rounded-full"
         style={{
           background: `conic-gradient(#ededed ${degAtual}deg, #4334c7 0deg`,
         }}
       >
-        <span className="text-white font-bold z-50">
-          {segundosRestantes} {degAtual}
-        </span>
+        <span className="text-white text-2xl font-bold z-50">{getTempoAtual()}</span>
       </div>
 
-      <button onClick={()=>cronometro(totalMinutos)} className="bg-custom-purple-hover px-6 py-2 rounded-md hover:scale-95 hover:bg-custom-purple-hover transition-all">
-        Iniciar
-      </button>
+      <div className="flex w-full justify-between max-w-[100px]">
+        <div className="play">
+          {!paused ? (
+            <span
+              onClick={() => {
+                start();
+              }}
+              className="cursor-pointer hover:text-custom-purple-hover transition-all"
+            >
+              <Play size={24} weight="fill" />
+            </span>
+          ) : (
+            <span
+              onClick={() => {
+                pause();
+              }}
+              className="cursor-pointer text-xl hover:text-custom-purple-hover transition-all"
+            >
+              <Pause size={24} weight="fill" />
+            </span>
+          )}
+        </div>
+        <div className="reload">
+          <span
+            onClick={() => {
+              reset();
+            }}
+            className="cursor-pointer hover:text-custom-purple-hover transition-all"
+          >
+            <RewindCircle size={24} weight="fill" />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/components/Todo/TodoComponent.tsx
+++ src/app/components/Todo/TodoComponent.tsx
@@ -2,8 +2,8 @@ import { Pencil, Play, WifiMedium } from "@phosphor-icons/react";
 
 export default function TodoComponent() {
   return (
-    <div className="w-full h-full px-2 flex flex-col gap-6 justify-center items-center">
-      <div className="container flex flex-col max-w-lg max-md:max-w-none gap-5">
+    <div className="w-full h-full px-2 max-[420px]:max-w-xs flex flex-col gap-6 justify-center items-center">
+      <div className=" flex flex-col max-w-lg max-md:max-w-none max-[420px]:max-w-xs gap-5">
         <header>
           <h1 className="text-2xl font-bold text-gray-300">Lista de Tarefas</h1>
           <p className="text-zinc-400 text-sm">
@@ -12,7 +12,7 @@ export default function TodoComponent() {
             completadas.
           </p>
         </header>
-        <div className="flex justify-between w-full h-10">
+        <div className="flex justify-between w-full h-10 gap-4">
           <button className="add bg-custom-purple-hover px-5  rounded-md hover:scale-95 hover:bg-custom-purple-hover transition-all">
             Adicionar
           </button>
@@ -35,232 +35,34 @@ export default function TodoComponent() {
             </select>
           </div>
         </div>
-        <div className="todos grid divide-y-4 divide-dark max-h-[400px] overflow-y-auto overflow-x-hidden  scrollbar scrollbar-thumb-custom-purple-hover scrollbar-w-1">
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
+        <div className="todos w-full grid divide-y-4 divide-dark max-h-[400px] overflow-y-auto overflow-x-hidden  scrollbar scrollbar-thumb-custom-purple-hover scrollbar-w-1">
+          {Array.from({ length: 5 }, (_, index) => (
+            <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full max-[420px]:max-w-xs ">
+              <div className="flex gap-2 ">
+                <input type="checkbox" className="check" id="check" />
+                <label
+                  htmlFor="check"
+                  className="overflow-hidden  max-w-xs max-[420px]:max-w-[180px] overflow-ellipsis"
+                >
+                  <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Vitae est eligendi nostrum. Error provident cumque odit
+                    consequuntur consectetur ab culpa sunt, quae pariatur illo
+                    iure fugit veritatis eum, voluptas porro.
+                  </span>
+                </label>
+              </div>
+              <div className="flex gap-3">
+                <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
+                  <Pencil weight="fill" />
                 </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
 
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
+                <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
+                  <Play weight="fill" />
                 </span>
-              </label>
+              </div>
             </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
-          <div className="todo  flex justify-between items-center gap-2 h-16 bg-[#060314] px-4 rounded-md w-full ">
-            <div className="flex gap-2">
-              <input type="checkbox" className="check" id="check" />
-              <label
-                htmlFor="check"
-                className="overflow-hidden  max-w-xs  overflow-ellipsis"
-              >
-                <span className=" text-sm font-bold text-gray-300  whitespace-nowrap ">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-                  est eligendi nostrum. Error provident cumque odit consequuntur
-                  consectetur ab culpa sunt, quae pariatur illo iure fugit
-                  veritatis eum, voluptas porro.
-                </span>
-              </label>
-            </div>
-            <div className="flex gap-3">
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Pencil weight="fill" />
-              </span>
-
-              <span className="cursor-pointer hover:text-custom-purple-hover transition-all">
-                <Play weight="fill" />
-              </span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
